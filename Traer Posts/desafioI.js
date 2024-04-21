a7de39158fa1a89fa8582579a6386a3cbae5bf9c
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-04-21 02:55:50 -0400

    Cambios archivo js

---
+++ Traer Posts/desafioI.js
@@ -0,0 +1,38 @@
+
+const obtenerPosts = async () => {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        if (response.status === 200) {
+            const data = await response.json();
+            mostrar(data);
+        }
+        //console.log(data);
+    } catch (error) {
+        console.error('Error al obtener datos', error);
+    }
+}
+
+const mostrar = (data) => {
+    try {
+        let posts = '';
+        data.forEach(element => {
+            posts += `
+            <ul>
+                <li>
+                    <p>User Id: ${element.userId} </p>
+                    <p>Id: ${element.id} </p>
+                    <p>Title: ${element.title} </p>
+                    <p>Body: ${element.body} </p>
+                </li>
+            </ul>
+            `
+        });
+        document.getElementById('post-data').innerHTML = posts;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const getPosts = () => {
+    obtenerPosts();
+}
